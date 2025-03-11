9938510350f7a109306685f0676ca3b6a9e6dcfc
IPVA <ivanpereira345@gmail.com>
2025-03-11 17:11:56 -0300

    Improving the package Util and the Class DateUtil, Creating the method formatLocalDateTimeToDatabaseStyle

--- src/main/java/learning/springboot/util/DateUtil.java
+++ src/main/java/learning/springboot/util/DateUtil.java
@@ -1,5 +1,11 @@
 package learning.springboot.util;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class DateUtil {
 
+    public String formatLocalDateTimeToDatabaseStyle(LocalDateTime localDateTime) {
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(localDateTime);
+    }
 }
